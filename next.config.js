a1e7574d588373aee5bf3c1acdf4fcfe408ffcf0
Matheus Costa <matheus@email.com>
2025-04-19 02:07:08 -0300

    Adicionar next.config.js compatível com a Vercel

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  experimental: {
+    // As configurações experimentais removidas por compatibilidade com a Vercel
+  },
+};
+
+module.exports = nextConfig; 
